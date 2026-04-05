4ee1a17ffaee88e45fdfb921b765e1e7327ce772
Jacob Williams <jacobwilliams@users.noreply.github.com>
2026-04-05 16:51:56 -0500

    updates

--- qonsole/__init__.py
+++ qonsole/__init__.py
@@ -4,8 +4,5 @@ This module provides an embeddable Python console widget for Qt applications.
 It supports syntax highlighting, command history, and autocompletion.
 """
 
-__version__ = "1.3.0"
+__version__ = "1.0.0"
 __description__ = "Lightweight python console, easy to embed into Qt applications"
-__author__ = "Marcus Oskarsson"
-__author_email__ = "marcus.oscarsson@esrf.fr"
-__url__ = "https://github.com/jacobwilliams/qonsole"
